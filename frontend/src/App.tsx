b552c04607cd77d0e90e284f4123273d03d1b03f
voidrlm <varjun609@gmail.com>
2026-02-08 01:42:57 -0500

    refactor: Update App.tsx to use MUI pages and remove old CSS files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
-import LandingPage from "./pages/LandingPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPageMUI";
+import FeaturesPage from "./pages/FeaturesPageMUI";
+import AboutPage from "./pages/AboutPageMUI";
+import PatientSignup from "./pages/PatientSignupMUI";
+import ProviderSignup from "./pages/ProviderSignupMUI";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/features" element={<FeaturesPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/signup/patient" element={<PatientSignup />} />
+        <Route path="/signup/provider" element={<ProviderSignup />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
